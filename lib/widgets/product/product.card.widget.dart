c7dccf5aa914381e7b37f7aacffa18b5741e041d
stoemilio <sto.emilio@gmail.com>
2020-05-22 21:51:37 -0300

    Término do módulo 3 - Organização

---
+++ lib/widgets/product/product.card.widget.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:shopping/pages/product.page.dart';
+
+class ProductCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String description;
+  final double price;
+
+  ProductCard({
+    @required this.image,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(5),
+      width: 170,
+      color: Colors.black12,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProductPage(
+                    title: this.title,
+                    description: this.description,
+                    image: this.image,
+                    price: this.price,
+                  ),
+                ),
+              );
+            },
+            child: Image.asset(
+              this.image,
+              width: 170,
+              height: 170,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 60,
+            child: Text(
+              this.title,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Container(
+            child: Text(
+              this.description,
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Container(
+            child: Text(
+              "\$ ${this.price}",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF00C569)),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
